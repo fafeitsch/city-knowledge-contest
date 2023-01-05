047bf1b78221b1b822974c93fda97dd980a39c35
Sonja Feitsch <feitsch@e2n.de>
2023-01-05 17:07:49 +0100

    frontend: remove old frontend and use svelte

---
+++ frontend/src/WaitingRoom.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import store from "./store";
+
+  let players: string[] = [];
+  onMount(() => {
+    store.get.players$.subscribe((value) => {
+      players = value;
+    });
+  });
+</script>
+
+<div>Waiting Room</div>
+<ul>
+  {#each players as player}
+    <li>{player}</li>
+  {/each}
+</ul>
